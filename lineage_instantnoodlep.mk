bf36fa946fb60b7bdb88dde68815b5c5333f8e53
zahid <zahidhasmani07@gmail.com>
2024-04-08 04:54:26 +0000

    adapt to matrixx

--- lineage_instantnoodlep.mk
+++ lineage_instantnoodlep.mk
@@ -13,13 +13,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/instantnoodlep/device.mk)
 
 # Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-EXTRA_UDFPS_ANIMATIONS := true
+#GAPPS
+WITH_GMS := true
+
+# Matrixx
+MATRIXX_BUILD_TYPE := OFFICIAL
+MATRIXX_MAINTAINER := zizzyboi
+MATRIXX_CHIPSET := SM8250
+MATRIXX_BATTERY := 4510mAh
+MATRIXX_DISPLAY := 1440x3168
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_EXCLUDES_AUDIOFX := true
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
+EXTRA_UDFPS_ANIMATIONS := true
 TARGET_SUPPORTS_QUICK_TAP := true
 
-PRODUCT_NAME := evolution_instantnoodlep
+PRODUCT_NAME := lineage_instantnoodlep
 PRODUCT_DEVICE := instantnoodlep
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
